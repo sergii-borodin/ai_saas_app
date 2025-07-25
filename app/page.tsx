0059a12dd4d2cfab96015c5ccc6385a3d8a853b5
Sergii Borodin <sergiiborodin@Sergiis-MacBook-Pro.local>
2025-07-25 17:40:04 +0200

    create static cards for popular companions section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,41 @@
-import { Button } from "@/components/ui/button";
-import React from "react";
+import CompanionCard from "@/components/CompanionsCards";
+import CompanionsList from "@/components/CompanionsList";
+import Cta from "@/components/Cta";
 
 const Page = () => {
   return (
     <div>
-      <Button>Button</Button>
+      <h1 className="text-2xl underline">Popular companions</h1>
+      <section className="home-section">
+        <CompanionCard
+          id="123"
+          name="Neura the Brainy Explorer"
+          topic="Neural Network of the Brain"
+          subject="Science"
+          duration={45}
+          color="#ffda6e"
+        />
+        <CompanionCard
+          id="456"
+          name="Countsy the Number Wizard"
+          topic="Derivatives & Integrals"
+          subject="Maths"
+          duration={30}
+          color="#e5d0ff"
+        />
+        <CompanionCard
+          id="789"
+          name="Verba the Vocabulary Builder"
+          topic="English Literature"
+          subject="Language"
+          duration={20}
+          color="#ffda6e"
+        />
+      </section>
+      <section className="home-section">
+        <CompanionsList />
+        <Cta />
+      </section>
     </div>
   );
 };
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-import NavItems from "./ui/NavItems";
+import NavItems from "./NavItems";
 
 const Navbar = () => {
   return (
