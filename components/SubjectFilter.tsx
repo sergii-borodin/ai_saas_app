420ad1c93a0707a6376ed23d4f6e8d0ec73afb71
Sergii Borodin <sergii.borodin@yahoo.com>
2025-08-06 12:26:13 +0200

    implement additional query params filtering by subject

--- components/SubjectFilter.tsx
+++ components/SubjectFilter.tsx
@@ -1,7 +1,56 @@
-import React from "react";
+"use client";
+
+import { useEffect, useState } from "react";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { recentSessions } from "@/constants";
 
 const SubjectFilter = () => {
-  return <div>SubjectFilter</div>;
+  const pathname = usePathname();
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const query = searchParams.get("subject") || "";
+
+  const [subjectQuery, setSubjectQuery] = useState(query);
+  useEffect(() => {
+    setSubjectQuery(query);
+  }, [query]);
+
+  useEffect(() => {
+    const params = new URLSearchParams(searchParams);
+    console.log("subjectQuery", subjectQuery);
+
+    if (subjectQuery.trim()) {
+      params.set("subject", subjectQuery.trim());
+    } else {
+      params.delete("subject");
+    }
+
+    const newUrl = `${pathname}?${params.toString()}`;
+    router.push(newUrl);
+  }, [subjectQuery, router, pathname, searchParams]);
+
+  return (
+    <Select onValueChange={(e) => setSubjectQuery(e)} value={subjectQuery}>
+      <SelectTrigger className="input capitalize">
+        <SelectValue placeholder="Subject" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectItem value="all">All Subjects</SelectItem>
+        {recentSessions.map(({ subject }) => (
+          <SelectItem key={subject} value={subject} className="capitalize">
+            {subject}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
+  );
 };
 
 export default SubjectFilter;
