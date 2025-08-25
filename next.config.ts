de3c29a8b67477d68c608d2f93cfd687af9e5760
Sergii Borodin <sergii.borodin@yahoo.com>
2025-08-25 15:37:28 +0200

    Add ESLint configuration to ignore errors during builds in Next.js

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  eslint: { ignoreDuringBuilds: true },
   images: {
     remotePatterns: [{ hostname: "img.clerk.com" }],
   },
